6109c0cec6a06319c791ac035688a0b63aa2a9bd
Svyatoslav <43129468+Svyatoslave@users.noreply.github.com>
2021-12-09 13:16:39 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Header} from "./components"
+import Home from "./pages/Home"
+import Cart from "./pages/Cart"
+
+import {Route} from "react-router-dom";
+
+
+function App() {
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+               <Route path='/' component={Home} exact/>
+                <Route path='/cart' component={Cart} exact/>
+            </div>
+        </div>
+
+    )
+}
+
+export default App;
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+
+
+function Cart() {
+
+    return (
+         <div>
+             <h1>Корзина</h1>
+         </div>
+    )
+}
+
+export default Cart
